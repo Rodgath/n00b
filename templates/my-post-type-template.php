66876997f27873b652c1d2668cd61c9282dd3b56
Peter Rodgath <rodgath@gmail.com>
2019-05-08 18:59:34 +0300

    Add files via upload

--- templates/my-post-type-template.php
+++ templates/my-post-type-template.php
@@ -4,7 +4,15 @@
 
 get_header(); 
 
-$container   = n00b_get_option('n00b_options', 'container_type', get_queried_object_id());
+$sidebar_pos = n00b_get_option('n00b_options', 'sidebar_position', get_queried_object_id(), array('metabox_prefix' => N00B_META_PREFIX));
+$layout_cols = n00b_get_option('n00b_options', 'layout_cols', get_queried_object_id(), array('metabox_prefix' => N00B_META_PREFIX));
+$layout_obj  = n00b_custom_col_class($layout_cols);
+
+extract($layout_obj); // Returns ($sl_custom_class, $sr_custom_class, $a_custom_class) variables
+
+$sidebar_left_col  = n00b_get_col_class($sidebar_pos, 'sidebar_left_col', $sl_custom_class);
+$sidebar_right_col = n00b_get_col_class($sidebar_pos, 'sidebar_right_col', $sr_custom_class);
+$article_col       = n00b_get_col_class($sidebar_pos, 'article_col', $a_custom_class);
 $sidebar_pos = n00b_get_option('n00b_options', 'sidebar_position', get_queried_object_id());
 
 $sidebar_left_col  = n00b_get_col_class($sidebar_pos, 'sidebar_left_col');
