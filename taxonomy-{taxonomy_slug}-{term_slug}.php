1551de2adabb0a047e2b9c1bba835380b6543a79
Peter Rodgath <rodgath@gmail.com>
2019-05-08 19:17:48 +0300

    Add files via upload

--- taxonomy-{taxonomy_slug}-{term_slug}.php
+++ taxonomy-{taxonomy_slug}-{term_slug}.php
@@ -32,7 +32,7 @@ $article_col       = n00b_get_col_class($sidebar_pos, 'article_col', $a_custom_c
 ?>
 
 	<!-- START Title section -->
-	<section id="title" class="mb-3">
+	<section id="title" class="pb-4 mb-4 border-bottom">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
