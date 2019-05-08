fb6104ce28eab71c62e6c236a4601304a627a24e
Peter Rodgath <rodgath@gmail.com>
2019-05-08 19:19:37 +0300

    Add files via upload

--- 404.php
+++ 404.php
@@ -24,7 +24,7 @@ $article_col       = n00b_get_col_class($sidebar_pos, 'article_col', $a_custom_c
 ?>
 
 	<!-- START Title section -->
-	<section id="title" class="pb-4 mb-4 border-bottom">
+	<section id="title" class="pb-4 mb-5 border-bottom">
 		<div class="container">
 			<div class="row">
 				<h1><?php _e('Error', 'n00b'); ?> 404</h1>
--- author-{id}.php
+++ author-{id}.php
@@ -29,7 +29,7 @@ $article_col       = n00b_get_col_class($sidebar_pos, 'article_col', $a_custom_c
 ?>
 
 	<!-- START Title section -->
-	<section id="title" class="pb-4 mb-4 border-bottom">
+	<section id="title" class="pb-4 mb-5 border-bottom">
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-12">
